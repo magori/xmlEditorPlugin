cba75eeb2eff9c376db33f4a75acb1fd4c1e467a
dma <dma@globaz.ch>
2018-05-16 11:29:08 +0200

    first commit

---
+++ src/main/java/ch/hesso/plugin/ManipulaterJsonImpl.java
@@ -0,0 +1,74 @@
+package ch.hesso.plugin;
+
+import ch.hesso.xmleditor.editdom.Element;
+import ch.hesso.xmleditor.editdom.Manipulater;
+import ch.hesso.xmleditor.editdom.ManipulaterType;
+import ch.hesso.xmleditor.persistence.Persister;
+import com.google.auto.service.AutoService;
+import com.google.gson.*;
+
+import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
+
+@AutoService(Manipulater.class)
+public class ManipulaterJsonImpl implements Manipulater {
+    private final Persister persister;
+    private JsonElement element;
+    private String idDocument;
+
+    public ManipulaterJsonImpl() {
+        this.persister = null;
+    }
+
+    @Inject
+    public ManipulaterJsonImpl(Persister persister) {
+        this.persister = persister;
+    }
+
+    @Override
+    public String forType() {
+        return ManipulaterType.JSON.getName();
+    }
+
+    @Override
+    public Element getRootElement() {
+        return new ElementJsonImpl(element);
+    }
+
+    @Override
+    public Element editElement(String id, String newText) {
+        String[] ids = id.split("-");
+        JsonObject jsonObject = element.getAsJsonObject();
+        for (int i = 0; i < (ids.length - 1); i++) {
+            String property = resolveProperty(ids[i], jsonObject);
+            jsonObject = jsonObject.get(property).getAsJsonObject();
+        }
+        String property = resolveProperty(ids[ids.length - 1], jsonObject);
+        jsonObject.addProperty(property, newText);
+        return new ElementJsonImpl(jsonObject.get(property));
+    }
+
+    private String resolveProperty(String id, JsonObject jsonObject) {
+        Map<Integer, String> indexToKey = new HashMap<>();
+        jsonObject.keySet().forEach(k -> indexToKey.put(indexToKey.size(), k));
+        return indexToKey.get(Integer.valueOf(id));
+    }
+
+    public void saveDocument() {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(element);
+        this.persister.save(this.idDocument, json);
+    }
+
+    @Override
+    public void load(String idDocument) {
+        this.idDocument = idDocument;
+        this.parse(this.persister.load(idDocument));
+    }
+
+    void parse(String json) {
+        JsonParser parser = new JsonParser();
+        this.element = parser.parse(json);
+    }
+}
